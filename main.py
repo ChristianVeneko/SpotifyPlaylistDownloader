12912d1fc4211775137fc157e442c1c54b8e430e
Christian Mendoza <christianmendozater@gmail.com>
2023-07-28 18:09:17 -0400

    Add Auth

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+# Crear un objeto de autenticación con el ID del cliente y el secreto del cliente
+auth_manager = SpotifyClientCredentials(client_id='tu_id_de_cliente', client_secret='tu_secreto_de_cliente')
+
+# Crear un objeto de la API de Spotify con el objeto de autenticación
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
