432d6d6fc245efcc3ad055c89a52933326a8bfe4
Christian Mendoza <christianmendozater@gmail.com>
2023-07-28 18:11:37 -0400

    Get the songs and artists from the playlist

--- main.py
+++ main.py
@@ -7,3 +7,30 @@ auth_manager = SpotifyClientCredentials(client_id='tu_id_de_cliente', client_sec
 # Crear un objeto de la API de Spotify con el objeto de autenticación
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
+# Definir el enlace de la playlist
+link = 'https://open.spotify.com/playlist/2ZH6fPLGbx4UkR4d6sSoU3'
+
+# Extraer el ID de la playlist del enlace
+playlist_id = link.split('/')[-1]
+
+# Crear una lista vacía para almacenar las canciones
+songs = []
+
+# Inicializar el offset y el límite
+offset = 0
+limit = 50
+
+# Obtener el número total de elementos de la playlist
+total = spotify.playlist_items(playlist_id, limit=1)['total']
+
+# Recorrer los elementos de la playlist por lotes de 50
+while offset < total:
+  # Obtener los elementos de la playlist con el offset y el límite actuales
+  results = spotify.playlist_items(playlist_id, offset=offset, limit=limit)
+  # Recorrer los resultados y extraer los datos relevantes
+  for item in results['items']:
+    song_name = item['track']['name']
+    artist_name = item['track']['artists'][0]['name']
+    songs.append((song_name, artist_name))
+  offset += limit
+
